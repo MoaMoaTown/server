e2514f04d475b12b8bd87c70ba2edf25724da298
wj0624 <wonjeong4538@gmail.com>
2024-08-25 00:17:50 +0900

    refactor: 타운 만들기 로직 수정

--- src/main/java/com/themore/moamoatown/town/dto/TownCreateInternalDTO.java
+++ src/main/java/com/themore/moamoatown/town/dto/TownCreateInternalDTO.java
@@ -6,16 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 타운 생성 Response DTO
+ * 타운 만들기 내부 DTO
  * @author 임원정
- * @since 2024.08.23
+ * @since 2024.08.24
  * @version 1.0
  *
  * <pre>
  * 수정일        수정자        수정내용
  * ----------  --------    ---------------------------
- * 2024.08.23  	임원정       최초 생성
- * 2024.08.24  	임원정       타운코드만 반환하도록 수정
+ * 2024.08.24  	임원정        최초 생성
  * </pre>
  */
 
@@ -23,6 +22,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class TownCreateResponseDTO {
+public class TownCreateInternalDTO {
+    private Long townId;
     private String townCode;
 }
--- src/main/java/com/themore/moamoatown/town/service/TownCodeGenerator.java
+++ src/main/java/com/themore/moamoatown/town/service/TownCodeGenerator.java
@@ -3,8 +3,22 @@ package com.themore.moamoatown.town.service;
 import java.security.SecureRandom;
 import java.util.Random;
 
+/**
+ * 타운 코드 생성 클래스
+ * @author 임원정
+ * @since 2024.08.23
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.23  	임원정        최초 생성
+ * 2024.08.24   임원정        문자 추가(대문자 + 숫자)
+ * </pre>
+ */
+
 public class TownCodeGenerator {
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int CODE_LENGTH = 8;
     private static final Random RANDOM = new SecureRandom();
 
