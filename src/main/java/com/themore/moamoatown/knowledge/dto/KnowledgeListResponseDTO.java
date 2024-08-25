d7e304a73d8bf9459c59e0a0797d606874be5831
JZU0 <ljzh1030@gmail.com>
2024-08-26 01:24:55 +0900

    feat: add 지식 리스트 조회

---
+++ src/main/java/com/themore/moamoatown/knowledge/dto/KnowledgeListResponseDTO.java
@@ -0,0 +1,30 @@
+package com.themore.moamoatown.knowledge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 지식 리스트 조회 Response DTO
+ * @author 이주현
+ * @since 2024.08.26
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.26  이주현        최초 생성
+ * </pre>
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class KnowledgeListResponseDTO {
+    private Long knowledgeId;
+    private String title;
+    private String content;
+    private String createdAt;
+}
