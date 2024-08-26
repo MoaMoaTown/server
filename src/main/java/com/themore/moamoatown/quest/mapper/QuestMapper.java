7a1b82f2279c49e5ef9bfccda3edd65f17f4b6c8
JZU0 <ljzh1030@gmail.com>
2024-08-26 14:18:51 +0900

    feat: add 퀘스트 리스트 조회

---
+++ src/main/java/com/themore/moamoatown/quest/mapper/QuestMapper.java
@@ -0,0 +1,25 @@
+package com.themore.moamoatown.quest.mapper;
+
+import com.themore.moamoatown.quest.dto.QuestResponseDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 퀘스트 매퍼 인터페이스
+ * @author 이주현
+ * @since 2024.08.26
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.26  이주현        최초 생성
+ * </pre>
+ */
+
+@Mapper
+public interface QuestMapper {
+    // 퀘스트 리스트 조회
+    List<QuestResponseDTO> findQuestsByTownId(Long townId);
+}
